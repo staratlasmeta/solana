22f66a84b532fa1e434d0bc5497e37b2e7fea6f1
HaoranYi <haoran.yi@gmail.com>
2022-04-01 13:47:06 -0500

    handle channel disconnect (#24036)
    (cherry picked from commit c9a476e24d9aa9da6e6bd37739f419ed42632c88)
    
    # Conflicts:
    #	rpc/src/rpc_completed_slots_service.rs

--- rpc/src/rpc_completed_slots_service.rs
+++ rpc/src/rpc_completed_slots_service.rs
@@ -1,5 +1,6 @@
 use {
     crate::rpc_subscriptions::RpcSubscriptions,
+    crossbeam_channel::RecvTimeoutError,
     solana_client::rpc_response::SlotUpdate,
     solana_ledger::blockstore::CompletedSlotsReceiver,
     solana_sdk::timing::timestamp,
@@ -17,6 +18,7 @@ impl RpcCompletedSlotsService {
     ) -> JoinHandle<()> {
         Builder::new()
             .name("solana-rpc-completed-slots-service".to_string())
+<<<<<<< HEAD
             .spawn(move || {
                 for slots in completed_slots_receiver.iter() {
                     for slot in slots {
@@ -24,6 +26,29 @@ impl RpcCompletedSlotsService {
                             slot,
                             timestamp: timestamp(),
                         });
+=======
+            .spawn(move || loop {
+                // received exit signal, shutdown the service
+                if exit.load(Ordering::Relaxed) {
+                    break;
+                }
+
+                match completed_slots_receiver
+                    .recv_timeout(Duration::from_millis(COMPLETE_SLOT_REPORT_SLEEP_MS))
+                {
+                    Err(RecvTimeoutError::Timeout) => {}
+                    Err(RecvTimeoutError::Disconnected) => {
+                        info!("RpcCompletedSlotService channel disconnected, exiting.");
+                        break;
+                    }
+                    Ok(slots) => {
+                        for slot in slots {
+                            rpc_subscriptions.notify_slot_update(SlotUpdate::Completed {
+                                slot,
+                                timestamp: timestamp(),
+                            });
+                        }
+>>>>>>> c9a476e24 (handle channel disconnect (#24036))
                     }
                 }
             })
